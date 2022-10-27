3820218219620db61d616ec8f3a9f25cff261912
Georgina Segundo <geprgina.segundo@hotmail.com>
2022-10-27 20:00:14 -0300

    finalizo diseño de pag responsive index y servicios, ingreso contendido en el resto del sitio

--- menu.js
+++ menu.js
@@ -3,8 +3,7 @@ $(document).ready(main);
 var contador = 1;
 
 function main(){
-    $('.menu').click(function(){
-        // $('nav').toggle(); 
+    $('.menu').click(function(){ 
 
         if(contador == 1){
             $('nav').animate({
